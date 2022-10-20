a0f5d1d5e52cc320f01595803acc4799a5fb35a7
Koen van Wijk <koen.van.wijk@asml.com>
2022-10-20 14:57:10 +0200

    Update code generator language

--- public/editors/code/editor.js
+++ public/editors/code/editor.js
@@ -2168,6 +2168,129 @@ Blockly.Blocks['generate_token_value'] = {
 
 };
 
+Blockly.Blocks['generate_code_variable'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldTextInput("YAML", null, ), "LANGUAGE")
+        .appendField("code for")
+        .appendField(new Blockly.FieldDropdown([["variables_get","variables_get"], ["variables_set","variables_set"]]), "TYPE");
+    this.appendStatementInput("CODE")
+        .setCheck(null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  },
+
+  /*
+   * Create XML to represent the output type.
+   * @return {!Element} XML storage element.
+   * @this {Blockly.Block}
+   */
+  mutationToDom: function() {
+    var container = Blockly.utils.xml.createElement('mutation');
+    var field;
+    for (var b = 0, input; input = this.inputList[b]; b++)
+    {
+      for (var d = 0, field; field = input.fieldRow[d]; d++)
+	    {	
+	      if (field.getOptions && !field.variable_) // is dropdown and not a variable
+		    {
+          var dropdown = Blockly.utils.xml.createElement('dropdown');
+          dropdown.setAttribute('field', field.name);
+        
+          container.appendChild(dropdown)
+          var options = field.getOptions()
+          for (var i = 0; i < options.length; i++) {
+            var option = Blockly.utils.xml.createElement('option');
+            option.setAttribute('text', options[i][0]);
+            option.setAttribute('id', options[i][1]);
+            dropdown.appendChild(option);
+          }
+    		}
+      }
+    }
+    return container;
+  },
+  saveExtraState: function() {
+    var field;
+    var state = {'dropdowns':[]};
+    for (var b = 0, input; input = this.inputList[b]; b++)
+    {
+      for (var d = 0, field; field = input.fieldRow[d]; d++)
+	    {	
+	      if (field.getOptions && !field.variable_) // is dropdown and not a variable
+		    {
+          var field_state = {'field':field.name, 'options' : []}
+          state.dropdowns.push(field_state);
+          var options = field.getOptions()
+          for (var i = 0; i < options.length; i++) {
+            var option_state = {'text': options[i][0], 'id':options[i][1]}
+            field_state.options.push(option_state)
+          }
+    		}
+      }
+    }
+    return state;
+  },
+
+  /**
+   * Parse XML to restore the output type.
+   * @param {!Element} xmlElement XML storage element.
+   * @this {Blockly.Block}
+   */
+  domToMutation: function(xmlElement) {
+
+    for (var i = 0, childNode; (childNode = xmlElement.childNodes[i]); i++) {
+      if (childNode.nodeName.toLowerCase() == 'dropdown') {
+        var field_name = childNode.getAttribute('field');
+        var field = this.getField(field_name);
+    
+        var options = field.getOptions(false)
+        var ids = options.map(option => option[1]);
+        
+        for (var j = 0, optionsElement; (optionsElement = childNode.childNodes[j]); j++) {
+          if (optionsElement.nodeName.toLowerCase() == 'option') {
+            var text = optionsElement.getAttribute('text');
+            var id = optionsElement.getAttribute('id');
+            if (!ids.includes(id)) {
+              options.push([text,id])
+            }
+          }
+        }
+        field.savedOptionsSet = true;     
+      }
+    }
+  },
+  loadExtraState: function(state) {
+    for (var i=0; i<state.dropdowns.length; i++)
+    {
+      var field_name = state.dropdowns[i].field;
+      var field = this.getField(field_name);
+      if (field.getOptions && !field.variable_) // is dropdown and not a variable
+      { 
+         var options = field.getOptions(false);
+      }
+      else
+      {
+        var options = []
+      }
+      var ids = options.map(option => option[1]);
+      for (var j =0; j<state.dropdowns[i].options.length;j++)
+      {
+        var text = state.dropdowns[i].options[j].text;
+        var id = state.dropdowns[i].options[j].id;
+        if (!ids.includes(id)) {
+          options.push([text,id])
+        }
+      }
+      field.savedOptionsSet = true;
+    }
+  }
+
+
+
+};
+
 if (!Blockly.JavaScript) {
   Blockly.JavaScript = new Blockly.Generator('JavaScript');
   Blockly.JavaScript.ORDER_ATOMIC = 0;
@@ -2526,6 +2649,10 @@ toolbox = {
     },
     {
       "kind": "block",
+      "type": "generate_code_variable"
+    },
+    {
+      "kind": "block",
       "type": "generate_token"
     },
     {
@@ -2599,6 +2726,14 @@ toolbox = {
     {
       "kind": "block",
       "type": "generate_token_if_next_block"
+    },
+    {
+      "kind": "block",
+      "type": "generate_block_type"
+    },
+    {
+      "kind": "block",
+      "type": "generate_code_variable"
     },
    ]
   },
