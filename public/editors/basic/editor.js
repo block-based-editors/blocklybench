500703d678824c56b86475b0bfec955912aa067e
Koen van Wijk <koen.van.wijk@asml.com>
2022-11-06 16:06:52 +0100

    Cleanup basic editor

--- public/editors/basic/editor.js
+++ public/editors/basic/editor.js
@@ -121,129 +121,6 @@ Blockly.Blocks['basic_list'] = {
 
 };
 
-Blockly.Blocks['model_cpd_name_value'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldLabelSerializable("CPD_NAME", null, ), "KEY")
-        .appendField(":")
-        .appendField(new Blockly.FieldTextInput("DIVEIN", null, ), "VALUE");
-    this.setPreviousStatement(true, "key_value");
-    this.setNextStatement(true, "key_value");
-    this.setColour(120);
- this.setTooltip("");
- this.setHelpUrl("");
-  },
-
-  /*
-   * Create XML to represent the output type.
-   * @return {!Element} XML storage element.
-   * @this {Blockly.Block}
-   */
-  mutationToDom: function() {
-    var container = Blockly.utils.xml.createElement('mutation');
-    var field;
-    for (var b = 0, input; input = this.inputList[b]; b++)
-    {
-      for (var d = 0, field; field = input.fieldRow[d]; d++)
-      {  
-        if (field.getOptions && !field.variable_) // is dropdown and not a variable
-        {
-          var dropdown = Blockly.utils.xml.createElement('dropdown');
-          dropdown.setAttribute('field', field.name);
-        
-          container.appendChild(dropdown)
-          var options = field.getOptions()
-          for (var i = 0; i < options.length; i++) {
-            var option = Blockly.utils.xml.createElement('option');
-            option.setAttribute('text', options[i][0]);
-            option.setAttribute('id', options[i][1]);
-            dropdown.appendChild(option);
-          }
-        }
-      }
-    }
-    return container;
-  },
-  saveExtraState: function() {
-    var field;
-    var state = {'dropdowns':[]};
-    for (var b = 0, input; input = this.inputList[b]; b++)
-    {
-      for (var d = 0, field; field = input.fieldRow[d]; d++)
-      {  
-        if (field.getOptions && !field.variable_) // is dropdown and not a variable
-        {
-          var field_state = {'field':field.name, 'options' : []}
-          state.dropdowns.push(field_state);
-          var options = field.getOptions()
-          for (var i = 0; i < options.length; i++) {
-            var option_state = {'text': options[i][0], 'id':options[i][1]}
-            field_state.options.push(option_state)
-          }
-        }
-      }
-    }
-    return state;
-  },
-
-  /**
-   * Parse XML to restore the output type.
-   * @param {!Element} xmlElement XML storage element.
-   * @this {Blockly.Block}
-   */
-  domToMutation: function(xmlElement) {
-
-    for (var i = 0, childNode; (childNode = xmlElement.childNodes[i]); i++) {
-      if (childNode.nodeName.toLowerCase() == 'dropdown') {
-        var field_name = childNode.getAttribute('field');
-        var field = this.getField(field_name);
-    
-        var options = field.getOptions(false)
-        var ids = options.map(option => option[1]);
-        
-        for (var j = 0, optionsElement; (optionsElement = childNode.childNodes[j]); j++) {
-          if (optionsElement.nodeName.toLowerCase() == 'option') {
-            var text = optionsElement.getAttribute('text');
-            var id = optionsElement.getAttribute('id');
-            if (!ids.includes(id)) {
-              options.push([text,id])
-            }
-          }
-        }
-        field.savedOptionsSet = true;     
-      }
-    }
-  },
-  loadExtraState: function(state) {
-    for (var i=0; i<state.dropdowns.length; i++)
-    {
-      var field_name = state.dropdowns[i].field;
-      var field = this.getField(field_name);
-      if (field.getOptions && !field.variable_) // is dropdown and not a variable
-      { 
-         var options = field.getOptions(false);
-      }
-      else
-      {
-        var options = []
-      }
-      var ids = options.map(option => option[1]);
-      for (var j =0; j<state.dropdowns[i].options.length;j++)
-      {
-        var text = state.dropdowns[i].options[j].text;
-        var id = state.dropdowns[i].options[j].id;
-        if (!ids.includes(id)) {
-          options.push([text,id])
-        }
-      }
-      field.savedOptionsSet = true;
-    }
-  }
-
-
-
-};
-
 Blockly.Blocks['basic_dict'] = {
   init: function() {
     this.appendDummyInput()
@@ -870,49 +747,6 @@ Blockly.JSON.scrub_ = function(block, code, opt_thisOnly) {
     return code + nextCode;
 }
 
-Blockly.JSON['basic_key_list'] = function(block) {
-  var code ='';
-  code += '"';
-  var field = block.getField('KEY');
-  if (field.getText()) {
-    code += field.getText();
-  } else {
-    code += field.getValue();
-  }
-  code += '" : "';
-  code += 'basic_key_list'
-  code += '," # key_value with KEY = "';
-  var field = block.getField('KEY');
-  if (field.getText()) {
-    code += field.getText();
-  } else {
-    code += field.getValue();
-  }
-  code += '" should create block with type: ';
-  code += 'basic_key_list'
-  code += '\n';
-
-  // if this block is a 'value' then code + ORDER needs to be returned
-  if(block.outputConnection) {
-    return [code, Blockly.JSON.ORDER_ATOMIC];
-  }
-  else // no value block
-  {
-    return code;
-  }
-}
-;
-if (!Blockly.JSON) {
-  Blockly.JSON = new Blockly.Generator('JSON');
-  Blockly.JSON.ORDER_ATOMIC = 0;
-}
-
-Blockly.JSON.scrub_ = function(block, code, opt_thisOnly) {
-    const nextBlock = block.nextConnection && block.nextConnection.targetBlock();
-    const nextCode = opt_thisOnly ? '' : Blockly.JSON.blockToCode(nextBlock);
-    return code + nextCode;
-}
-
 Blockly.JSON['basic_dict'] = function(block) {
   var code ='';
   code += '{\n';
@@ -1454,30 +1288,30 @@ BlocklyStorage.alert = function(message) {
 toolbox = {
  "kind": "flyoutToolbox",
  "contents": [
-    {
-      "kind": "block",
-      "type": "basic_list"
-    },
-    {
-      "kind": "block",
-      "type": "basic_dict"
-    },
-    {
-      "kind": "block",
-      "type": "basic_list_value"
-    },
-    {
-      "kind": "block",
-      "type": "basic_key_value"
-    },
-    {
-      "kind": "block",
-      "type": "basic_key_list"
-    },
-    {
-      "kind": "block",
-      "type": "basic_key_dict"
-    },
+  {
+    "kind": "block",
+    "type": "basic_list"
+  },
+  {
+    "kind": "block",
+    "type": "basic_dict"
+  },
+  {
+    "kind": "block",
+    "type": "basic_list_value"
+  },
+  {
+    "kind": "block",
+    "type": "basic_key_value"
+  },
+  {
+    "kind": "block",
+    "type": "basic_key_list"
+  },
+  {
+    "kind": "block",
+    "type": "basic_key_dict"
+  },
  ]
 };
     
@@ -1508,10 +1342,10 @@ var options = {
 };
 
 function codeGeneration(event) {
-  if (Blockly.JSON)
+  if (Blockly.YAML)
   {  
     try {
-          var code = Blockly.JSON.workspaceToCode(workspace);
+          var code = Blockly.YAML.workspaceToCode(workspace);
     } catch (e) {
       console.warn("Error while creating code", e);
       code = "Error while creating code:" + e
